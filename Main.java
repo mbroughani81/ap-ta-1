b66d58b11783c27d69a212ef306894b96d2e1cfc
Mohammad Broughani <gitmbroughani81@gmail.com>
2023-02-18 22:29:45 +0330

    scoping

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import p1.Student;
 
 public class Main{
+    String id = "class-level"; 
+
     public static void main(String[]args) {
         Student s = new Student("aidin", "1380/1/1", "12345");
         
@@ -9,5 +11,9 @@ public class Main{
         // System.out.println(s.birthday); 
         // ERROR
         // System.out.println(s.id);
+
+    
     }
+
+
 }
--- p1/Student.java
+++ p1/Student.java
@@ -4,10 +4,23 @@ public class Student {
     public String name;
     String birthday;
     private String id;
-
-    public Student(String name, String birthday, String id) {
+    private String scope;
+    
+    public Student(String name, String birthday, String id, String scope) {
         this.name = name;
         this.birthday = birthday;
         this.id = id;
+        this.scope = scope;
+    }
+
+    public void f1() {
+        System.out.println("class level is => " + scope);
+
+        {
+            String scope = "block-level";
+            System.out.println("block level is => " + scope);
+        }
+        // value is unchanged
+        System.out.println("class level is => " + scope);
     }
 }
